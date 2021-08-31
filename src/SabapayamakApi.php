cde71bbb0de981dd7512ca3ea40cd02650398680
Majid Dehnamaki <majid1991dehnamaki@gmail.com>
2021-08-31 19:00:25 +0430

    add send sms without token

--- src/SabapayamakApi.php
+++ src/SabapayamakApi.php
@@ -75,6 +75,10 @@ class SabapayamakApi
         {
             return sprintf($this->apiPath."/api/v1/message");
         }
+        if($method == "postMessageWithoutToke")
+        {
+            return sprintf($this->apiPath."/api/v1/message");
+        }
         if($method == "getDeliveriesById")
         {
             return sprintf($this->apiPath."/api/v1//api/v1/deliveries");
@@ -219,7 +223,40 @@ class SabapayamakApi
             return $e->getMessage();
           }  
     }
+    protected function Get($url)
+    {        
+        try{
+            $headers       = array(
+                'Accept: application/json',
+                'Content-Type: application/json',
+                'charset: utf-8'
+            );
+            $handle = curl_init();
+            curl_setopt($handle, CURLOPT_URL, $url);
+            curl_setopt($handle, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, false);
+            curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
 
+            $response     = curl_exec($handle);
+            $code         = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+            $content_type = curl_getinfo($handle, CURLINFO_CONTENT_TYPE);
+            $curl_errno   = curl_errno($handle);
+            $curl_error   = curl_error($handle);
+            if ($curl_errno) {
+                throw new Exception($curl_error);
+            }
+            $json_response = json_decode($response);
+            if(is_null($json_response)){
+                throw new Exception("Error happend!");
+            } else {
+                return $json_response;
+            }
+        }
+        catch(Exception $e) {
+            return $e->getMessage();
+          }  
+    }
 
 
 
@@ -284,6 +321,14 @@ class SabapayamakApi
        return $this->PostWithToken($path, $params, $token);
 
     }
+    public function SendMessage($username,$password,$from,$numbers,$text){
+        $path   = $this->get_path("postMessageWithoutToke")."/"."?UserName=".$username."&Password=".$password."&From=".$from."&To=".$numbers."&Text=".$text;
+
+       return $this->Get($path);
+
+    }
+
+    
     public function GetDeliveriesById($id,$token){
         $path   = $this->get_path("getDeliveriesById")."/".$id;
         return $this->GetWithToken($path, $token);
